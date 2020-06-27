ecce2b4ca503ff9138555144ed2b8ad6da12224f
Kamanashis-Biswas <kamanashisbiswas79@gmail.com>
2020-06-28 02:46:51 +0600

    Done

--- app/src/androidTest/java/com/nwu/studentsupport/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nwu/studentsupport/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.jhm69.cse11", appContext.getPackageName());
+        assertEquals("com.nwu.studentsupport", appContext.getPackageName());
     }
 }
